575cd25eb761e263c3d196731cc9561bad717076
Limon sheikh <cmtlover111@gmail.com>
2025-01-28 14:27:21 +0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-// import AiForm from "./pages/AiForm";
+
 import LoginAndSignupPage from "./pages/my pages/LoginAndSignupPage"; //login and sign up page 
 
 const App = () => {
   return (
     <>
       <LoginAndSignupPage/> 
-      {/* <AiForm/> */}
     </>
   );
 };
